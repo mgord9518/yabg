17abb3c8a115c727846bae765079569764bd79b2
Mathew Gordon <mgord9518@ubuntu-22-04.fedora>
2022-12-09 20:21:11 -0700

    Package resources with distribution

--- resources/build.sh
+++ resources/build.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/Not-Nik/raylib-zig --recurse-submodules
 git clone https://github.com/mgord9518/perlin-zig
 
+# Raylib hasn't yet been updated to latest Zig
 sed 's/addIncludeDir/addIncludePath/g' -i raylib-zig/raylib/src/build.zig
 
 zig build -Drelease-fast -Dtarget=x86_64-windows
@@ -10,3 +11,6 @@ zig build -Drelease-fast
 
 mv zig-out/bin/yabg.exe ./
 mv zig-out/bin/yabg     ./
+
+zip -9 yabg-x86_64-win.zip yabg.exe resources
+tar -cJvf yabg-x86_64-lin.zip yabg resources
