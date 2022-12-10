91442cc7986c9980a1000d383a8f2859630921ec
Mathew Gordon <mgord9518@ubuntu-22-04.fedora>
2022-12-09 20:12:56 -0700

    Fix deprecated call in Raylib build.zig

--- resources/build.sh
+++ resources/build.sh
@@ -3,6 +3,8 @@
 git clone https://github.com/Not-Nik/raylib-zig --recurse-submodules
 git clone https://github.com/mgord9518/perlin-zig
 
+sed 's/addIncludeDir/addIncludePath/g' -i raylib-zig/raylib/src/build.zig
+
 zig build -Drelease-fast -Dtarget=x86_64-windows
 zig build -Drelease-fast
 
